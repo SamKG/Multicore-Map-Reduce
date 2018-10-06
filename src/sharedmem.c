28f468331b041d4b061cd22234791e1e8c056e6e
samkg <samyak.k.gupta@gmail.com>
2018-10-05 23:28:54 -0400

    Added general purpose SHM storage

--- src/sharedmem.c
+++ src/sharedmem.c
@@ -13,6 +13,8 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sharedmem.h>
+#include <pthread.h>
+#include <stdio.h>
 
 /**
 * Gets a fd for the shared memory, given a name and size. If none exists, it creates it first.
@@ -48,4 +50,59 @@ char* append_string(char* one, char* two){
 	strcpy(newstr, one);
 	strcat(newstr,two);
 	return newstr;
+}
+
+
+
+/* GENERAL PURPOSE SHARED MEMORY SPACES */
+int general_shm_pos = 0;
+int general_shm_size = -1;
+int general_shm_fd = 0;
+void* general_shm_ptr = NULL;
+pthread_mutex_t general_shm_mutex;
+pthread_mutexattr_t general_shm_mutexattr;
+
+void shm_init_general(int size){
+	printf("INITIALIZING GENERAL_SHM\n");
+	int exists_flag = 0;
+	int shared_fd = get_shared_fd("GENERAL_SHM",size, &exists_flag);
+	general_shm_fd = shared_fd;
+	if (exists_flag){
+		return;
+	}
+	pthread_mutexattr_init(&general_shm_mutexattr);
+	pthread_mutexattr_setpshared(&general_shm_mutexattr, PTHREAD_PROCESS_SHARED);
+
+	pthread_mutex_init(&general_shm_mutex, &general_shm_mutexattr);
+
+	general_shm_ptr = mmap(NULL, size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED,shared_fd,0);
+	general_shm_size = size;	
+	printf("DONE\n");
+}
+
+void shm_resize_general(int size){
+	printf("SHM_GENERAL: RESIZE SPACE TO %d\n",general_shm_size + (size*10));
+	ftruncate(general_shm_fd,general_shm_size + (size*10));
+	general_shm_ptr = mremap(general_shm_ptr, general_shm_size, general_shm_size + (size*10),0,NULL);
+	general_shm_size += (size*10);
+}
+
+void shm_destroy_general(){
+	shm_unlink("GENERAL_SHM");
+	munmap(general_shm_ptr,general_shm_size);
+}
+/**
+* Lol this is basically a lazy malloc function, written to use shared memory
+*/
+int shm_get_general(int size){
+	pthread_mutex_lock(&general_shm_mutex);
+	printf("SHM_GENERAL: ALLOC %d SPACE\n",size);
+	if (general_shm_pos + size >= general_shm_size){
+		/* have to resize shared space */
+		shm_resize_general(size);
+	}
+	int data_offset = general_shm_pos;
+	general_shm_pos += size;
+	pthread_mutex_unlock(&general_shm_mutex);
+	return data_offset;
 }
