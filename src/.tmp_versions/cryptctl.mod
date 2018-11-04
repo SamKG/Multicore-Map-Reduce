2fa033a0fe61c4bfb2b204aa5417db7bb2ff6ac6
SamKG <samyak.k.gupta@gmail.com>
2018-11-04 16:32:25 -0500

    Module init and cleanup funcs

--- src/.tmp_versions/cryptctl.mod
+++
@@ -1,3 +0,0 @@
-/home/superuser/CS-416-F2018/src/cryptctl.ko
-/home/superuser/CS-416-F2018/src/cryptctl.o
-
--- src/cryptctl.c
+++ src/cryptctl.c
@@ -5,9 +5,36 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #define DEVICE_NAME "cryptctl"
-#define CLASS_NAME "cryptctl"
+#define CLASS_NAME "crypt"
+static int	majorNumber;
+static struct	class* cryptClass = NULL;
+static struct	device* cryptControllerDevice = NULL;
+
+static int	dev_open(struct inode*, struct file*);
+static int	dev_release(struct inode*, struct file*);
+static ssize_t	dev_read(struct file*, char*, size_t, loff_t*);
+static ssize_t	dev_write(struct file*,	const char*, size_t, loff_t*);
+static long	dev_ioctl(struct file*,unsigned int,unsigned long);
+
+static struct file_operations fops =
+{
+	.open = dev_open,
+	.release = dev_release,
+	.read = dev_read,
+	.write = dev_write,
+	.unlocked_ioctl = dev_ioctl,
+};
+
+static int __init crypt_init(void){
+	printk(KERN_ALERT "Crypt: Initializing...");
+	return 0;
+}	
+static void __exit crypt_cleanup(void){
+	printk(KERN_ALERT "Crypt: Cleaning up...");
+}
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Samyak K. Gupta");
 MODULE_DESCRIPTION("Cryptographic Linux Kernel Module for CS416");
 MODULE_VERSION("0.1");
+
