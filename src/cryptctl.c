f4d39b515e8837cfb6d37939c513d6db15732b6e
root <root@classvm165.cs.rutgers.edu>
2018-11-08 19:46:38 -0500

    added encrypt worker handlers

--- src/cryptctl.c
+++ src/cryptctl.c
@@ -12,12 +12,31 @@
 #define MAX_DEVICE_COUNT 1024
 #define PRIVATE_DATA_SIZE 4096
 
+//command defines
+#define CRYPTCTL_CREATE 1
+
+typedef struct cryptworker{
+	dev_t encrypter_dev_t;
+	dev_t decrypter_dev_t;
+	struct cdev* encrypter;
+	struct cdev* decrypter;
+} cryptworker;
+
+typedef struct file_private_data{
+	int type;
+	char* cipher;
+	int cipher_size;
+	char* data;
+	int data_size;
+	int data_count;
+}file_private_data;
+
+
 static int	majorNumber;
 static int	minorCount;
 static struct	class* cryptClass = NULL;
 //static struct	device* cryptctlCdev = NULL;
 static struct	cdev*   cryptctlCdev = NULL;
-static struct	cdev*	cryptCdev = NULL;
 
 static int	ctl_open(struct inode*, struct file*);
 static int	ctl_release(struct inode*, struct file*);
@@ -27,8 +46,10 @@ static long	ctl_ioctl(struct file*,unsigned int,unsigned long);
 
 static int	worker_open(struct inode*, struct file*);
 static int	worker_release(struct inode*, struct file*);
-static ssize_t	worker_read(struct file*, char*, size_t, loff_t*);
-static ssize_t	worker_write(struct file*,	const char*, size_t, loff_t*);
+static ssize_t	encrypt_worker_read(struct file*, char*, size_t, loff_t*);
+static ssize_t	encrypt_worker_write(struct file*,	const char*, size_t, loff_t*);
+static ssize_t	decrypt_worker_read(struct file*, char*, size_t, loff_t*);
+static ssize_t	decrypt_worker_write(struct file*,	const char*, size_t, loff_t*);
 static long	worker_ioctl(struct file*,unsigned int,unsigned long);
 
 static dev_t	workers[MAX_DEVICE_COUNT];
@@ -41,12 +62,20 @@ static struct file_operations fops =
 	.unlocked_ioctl = ctl_ioctl,
 };
 
-static struct file_operations workerfops =
+static struct file_operations encrypt_workerfops =
 {
 	.open = worker_open,
 	.release = worker_release,
-	.read = worker_read,
-	.write = worker_write,
+	.read = encrypt_worker_read,
+	.write = encrypt_worker_write,
+	.unlocked_ioctl = worker_ioctl,
+};
+static struct file_operations decrypt_workerfops =
+{
+	.open = worker_open,
+	.release = worker_release,
+	.read = decrypt_worker_read,
+	.write = decrypt_worker_write,
 	.unlocked_ioctl = worker_ioctl,
 };
 static int __init crypt_init(void){
@@ -84,26 +113,11 @@ static int __init crypt_init(void){
 	device_create(cryptClass, NULL, MKDEV(majorNumber,0), NULL, DEVICE_NAME);
 	cdev_add(cryptctlCdev, MKDEV(majorNumber,0), 1);	
 	
-	cryptCdev = cdev_alloc();
-	cdev_init(cryptCdev, &workerfops);
-	if (IS_ERR(cryptCdev)){
-		kobject_put(&cryptCdev->kobj);
-		cryptCdev = NULL;
-		kobject_put(&cryptctlCdev->kobj);
-		cryptctlCdev = NULL;
-		class_destroy(cryptClass);
-		unregister_chrdev(majorNumber, DEVICE_NAME);	
-		printk(KERN_ALERT "Crypt: Wasn't able to create worker cdev!\n");
-		return PTR_ERR(cryptCdev);
-	}
-	cryptCdev->owner = THIS_MODULE;
-
 	printk(KERN_WARNING "Crypt: Finished initializing!\n");
 	return 0;
 }	
 static void __exit crypt_exit(void){
 	printk(KERN_WARNING "Crypt: Cleaning up...\n");
-	cdev_del(cryptCdev);
 	device_destroy(cryptClass, MKDEV(majorNumber,0));
 	cdev_del(cryptctlCdev);
 	class_destroy(cryptClass);
@@ -129,13 +143,24 @@ static ssize_t ctl_write(struct file* filp, const char* msg, size_t strsize, lof
 	return 0;
 }
 static long ctl_ioctl(struct file* filp,unsigned int cmd,unsigned long arg){
+	switch(cmd){
+	case CRYPTCTL_CREATE:
+		printk(KERN_WARNING "Crypt: Creating new encrypt/decrypt pair!\n");
+		break;
+	default:
+		break;
+	}
 	return 0;
 }
 //define worker methods below
 static int worker_open(struct inode* inode, struct file* filp){
 	try_module_get(THIS_MODULE);
-	void* dataptr = kmalloc(sizeof(char) * PRIVATE_DATA_SIZE,0);
+	file_private_data* dataptr = (file_private_data*) kmalloc(sizeof(file_private_data),0);
 	filp->private_data = dataptr;
+	dataptr->type = 0;
+	dataptr->data = (char*) kmalloc(sizeof(char),0); 
+	dataptr->data_size = 1;
+	dataptr->data_count = 0;
 	printk(KERN_WARNING "Crypt worker: Opened!\n");
 	return SUCCESS;
 }
@@ -146,12 +171,55 @@ static int worker_release(struct inode* inode, struct file* filp){
 	printk(KERN_WARNING "Crypt worker: Released!\n");
 	return SUCCESS;
 }
-static ssize_t worker_read(struct file* filp, char* buff, size_t readsize, loff_t* filepos){
-	return 0; 
+char vigenere_encrypt(char key, char value){
+	int isCap = 0;
+	if(value >= ' ' && value <= '~'){
+		char tmp = value - ' ';
+		tmp = (tmp + key) % 95;
+		return (char) (tmp+' ');
+	}
+	return value;
 }
-static ssize_t worker_write(struct file* filp, const char* msg, size_t strsize, loff_t* filepos){
-	
-	return 0;
+static ssize_t encrypt_worker_read(struct file* filp, char* buff, size_t readsize, loff_t* fileoff){
+	int filepos = filp->f_pos;
+	file_private_data* dat = (file_private_data*) (filp->private_data);
+	if(dat == NULL || dat->cipher == NULL || dat->data == NULL || readsize == 0){
+		return 0;
+	}
+	char* tmp = (char*) kmalloc(sizeof(char)*readsize,0);
+	int i = 0;
+	for (i = 0 ; i < readsize ; i++){
+		int currpos = i + filepos;
+		if (dat->data_count < currpos){
+			break;
+		}
+		tmp[i] = vigenere_encrypt(dat->cipher[i%dat->cipher_size],dat->data[currpos]);
+	}
+	copy_to_user(buff,tmp,i);	
+	kfree(tmp);
+	return i; 
+}
+static ssize_t encrypt_worker_write(struct file* filp, const char* msg, size_t strsize, loff_t* fileoff){	
+	int filepos = filp->f_pos;
+	file_private_data* dat = (file_private_data*) (filp->private_data);
+	if(dat == NULL || dat->cipher == NULL || dat->data == NULL || strsize == 0){
+		return 0;
+	}
+	char* tmp = (char*) kmalloc(sizeof(char)*strsize,0);	
+	copy_from_user(tmp,msg,strsize);
+	if (filepos + strsize > dat->data_size){
+		krealloc(dat->data,(filepos + strsize)*2*sizeof(char),0);
+		dat->data_size = (filepos + strsize)*2;
+	}	
+	int i = 0;
+	for (i = 0 ; i < strsize ; i++){
+		dat->data[filepos+i] = tmp[i];
+		if (filepos + i > dat->data_count){
+			dat->data_count = filepos + i;
+		}
+	}
+	kfree(tmp);
+	return i;
 }
 static long worker_ioctl(struct file* filp,unsigned int cmd,unsigned long arg){
 	return 0;
